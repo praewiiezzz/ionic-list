685678c06015befb6fc5bc818d398c4af1d498b0
praewiiezzz <chutikan_praew@hotmail.com>
2018-05-23 14:21:40 +0700

    create friend list

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+
+  // Simple Array here...
+  numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  friendArray = [
+    {name:'Ant', phoneNumber:'011-111-111', imageUrl:'assets/imgs/ant.png'  }
+    ,{name:'Bird', phoneNumber:'022-222-222', imageUrl:'assets/imgs/bird.png'  }
+    ,{name:'Cat', phoneNumber:'1333', imageUrl:'assets/imgs/cat.png'  }
+    ,{name:'Dog', phoneNumber:'044-444-444', imageUrl:'assets/imgs/dog.png'  }
+    ,{name:'Egg', phoneNumber:'555', imageUrl:'assets/imgs/egg.png'  }
+  ];
+
   constructor(public navCtrl: NavController) {
 
   }
+
 
 }
